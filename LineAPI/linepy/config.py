c9ce5c526ae8c137fe8e778e3ff87502860dbbce
chemot <40890269+chemot@users.noreply.github.com>
2018-07-28 03:23:47 +0700

    Update config.py

--- LineAPI/linepy/config.py
+++ LineAPI/linepy/config.py
@@ -29,10 +29,10 @@ class Config(object):
     }
 
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
-    APP_VER     = '8.4.1'
+    APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'HelloWorld'
-    SYSTEM_VER  = '8.22.17'
+    SYSTEM_NAME = 'CHROMEOS'
+    SYSTEM_VER  = '2.1.5'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
